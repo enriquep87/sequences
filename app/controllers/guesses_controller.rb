94c97537087744343424554a945cd3972528e6da
Enrique P <ername@users.noreply.github.com>
2016-10-29 12:26:51 -0500

    Test 2

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -4,6 +4,16 @@ def index
   @second_number=params["second_num"]
   @third_number=params["third_num"]
 
+  g=Guess.new
+  g.first_num=params["first_num"]
+  g.second_num=params["second_num"]
+  g.third_num=params["third_num"]
+  g.save
+
+
+  @list=Guess.all
+
+
   if @first_number.to_i < @second_number.to_i && @second_number.to_i <@third_number.to_i
     @answer="Yes!"
   elsif
